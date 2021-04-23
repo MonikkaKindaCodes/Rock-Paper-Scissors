76b2315b6b13fccfff58d76c7e1989bda4a8226b
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-04-23 11:20:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Pulling buttons in DOM
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorBtn = document.querySelector('.scissors');
+const resultText = document.querySelector('.result');
+
+//Setting Computer Play
+const computerPlay = function () {
+    const selection = ['Rock', 'Paper', 'Scissors'];
+    const Math.floor(Math.random() * selection.length);
+}
+
+
+
+//Playing a round of Rock, Paper, Scissors again Computer
+
+const playRound = function (playerSelection, computerSelection) {
+
+}
+const playerSelection = 'rock';
